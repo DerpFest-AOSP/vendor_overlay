2198fca3aa38a75f11bf78ff8e4096f475b196b4
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-03-10 11:57:43 +0100

    overlay: Update Oppo Sans and OnePlus Sans [1/2]
    Oppo Sans: https://dumps.tadiphone.dev/dumps/oplus/ossi/-/blob/qssi-user-13-TP1A.220905.001-1677687796472-release-keys/system/system/fonts/SysSans-En-Regular.ttf
    OnePlus Sans: https://dumps.tadiphone.dev/dumps/oplus/ossi/-/blob/qssi-user-13-TP1A.220905.001-1677687796472-release-keys/system/system/fonts/OPSans-En-Regular.ttf
    
    Change-Id: I9dfc28b459cc80d0e6a08b2a870860f0535acd21

--- overlays.mk
+++ overlays.mk
@@ -41,7 +41,7 @@ PRODUCT_PACKAGES += \
     FontLGSmartGothicSourceOverlay \
     FontMontserratSourceOverlay \
     FontNothingDotOverlay \
-    FontOneplusSansOverlay \
+    FontOnePlusSansOverlay \
     FontOneplusSlateSource \
     FontOppoSansOverlay \
     FontFluidSansOverlay \
